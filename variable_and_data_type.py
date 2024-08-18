6f6d95ede1d8c9cd9859747db1f6fef5a8f0f887
LengTech11 <soklenghoungofficial11@gmail.com>
2024-08-18 21:46:02 +0700

    python full crash course

---
+++ variable_and_data_type.py
@@ -0,0 +1,76 @@
+# String
+# name = "Leng" + str(40) #can be in signle or double quote
+
+# Integer
+# age = 20
+# Explicit
+# age = int(30)
+
+# Double or Float
+# weigth = 50.5
+
+# Boolean
+# isMale = True
+# isMale = bool(30)
+
+# print (type(name))
+# print (type(age))
+# print (type(weigth))
+# print (type(isMale))
+
+# print (isMale)
+
+# Define variable 
+# hisName = "Leng"
+# _hisName = "Leng"
+# hisname = "Leng"
+# HisName = "Leng"
+
+# name,age,year = "John", 20 , "2003"
+# name = age = year = 2003
+# print (name)
+# print (age)
+# print (year)
+
+# Error Define 
+# 123hisName = "Leng"
+# his name = "Leng"
+
+# firstName = "Sok"
+# lastName = "Leng"
+
+# print (firstName,lastName)
+# print (firstName + lastName)
+
+# num1 = 10
+# num2 = 20
+
+# print (num1 + num2) # 30
+# print (num1,num2) # 10 20
+
+
+# global Function
+
+# example 1
+# name = "Leng"
+
+# def myFun():
+#     print (name)
+
+# myFun()
+
+# example 2 
+# def myFun():
+#     name = "lengTech"
+#     print (name)
+
+# myFun()
+
+# example 3 
+# def myFun():
+#     global name
+#     name = "Leng"
+#     # print(name)
+
+# myFun()
+# print (name)
